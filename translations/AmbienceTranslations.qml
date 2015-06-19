ef0a3cf9ca08d6dea7b7dbf4c04c74cfdfdaf8c8
0312birdzhang <0312birdzhang@gmail.com>
2015-06-19 18:57:23 +0800

    init

---
+++ translations/AmbienceTranslations.qml
@@ -0,0 +1,10 @@
+import QtQuick 2.0
+
+Item {
+    function qsTrIdString() {
+        //% "Ambience Bug"
+        QT_TRID_NOOP("ambience-bug")
+        //% "Ringtone by Kanong"
+        QT_TRID_NOOP("kanong")
+    }
+}
---
+++ translations/ambience-bug.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="ambience-bug">
+        <location filename="AmbienceTranslations.qml" line="6"/>
+        <source>Ambience Bug</source>
+        <translation>Bug一样的氛围</translation>
+    </message>
+    <message id="kanong">
+        <location filename="AmbienceTranslations.qml" line="8"/>
+        <source>Ringtone by Kanong</source>
+        <translation>铃声</translation>
+    </message>
+</context>
+</TS>
